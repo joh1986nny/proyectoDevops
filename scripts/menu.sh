63b012099c092e4aadb4efe734ddac0427adfd2d
joh1986nny <johnny.mm.86@gmail.com>
2021-04-19 12:37:40 +0200

    add visualización del mení de nuevo

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # Define la lista del menú aqui
 PS3="Selecciona una operación: " 
-select opt in "IP Publica" "Listado de Registros - dig" "Listener Puerto 443 - netcat" "Listado Puertos y Conexiones" "Tabla de Rutas" salir; 
+select opt in "IP Publica" "Listado de Registros - dig" "Listener Puerto 443 - netcat" "Listado Puertos y Conexiones" "Tabla de Rutas" "Ver Menú" salir; 
 do 
     case $opt in 
         "IP Publica") 
@@ -24,6 +24,16 @@ do
         "Tabla de Rutas") 
             ./routing_table/script.sh
             echo ""
+            ;;
+        "Ver Menú")
+            echo ""
+            echo "1) IP Publica" 
+            echo "2) Listado de Registros - dig" 
+            echo "3) Listener Puerto 443 - netcat" 
+            echo "4) Listado Puertos y Conexiones" 
+            echo "5) Tabla de Rutas" 
+            echo "6) Ver Menú"
+            echo "7) Salir"
             ;;
         salir) 
             break 
